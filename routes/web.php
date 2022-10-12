135785edfe0c8dd21dd951795c747fef40f74c9a
xLasTxqq <xlastx.qq@gmail.com>
2022-10-12 19:42:37 +0300

    Changed README

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\RegisterController;
-use App\Models\Application;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,21 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome'); 
-// });
-Route::get('/',[MainController::class,'index'])->name('main');
-Route::get('/count',[MainController::class,'count'])->name('count');
-Route::middleware('guest')->group(function(){
-    Route::resource('login',LoginController::class);
-    Route::resource('register',RegisterController::class);
-});
-Route::middleware('auth')->group(function(){
-    Route::resource('application',ApplicationController::class)->only(['index','create','store','destroy']);
-    Route::middleware('admin')->resource('category',CategoryController::class);
-    Route::middleware('admin')->resource('application',ApplicationController::class)->only(['update']);
-    Route::middleware('admin')->get('admin',[AdminController::class,'index'])->name('admin');
-    Route::get('/logout',[LoginController::class,'logout'])->name('logout');
+Route::get('/', [MainController::class, 'index'])->name('main');
+Route::get('/count', [MainController::class, 'count'])->name('count');
+
+Route::middleware('guest')->group(function () {
+    Route::resource('login', LoginController::class);
+    Route::resource('register', RegisterController::class);
 });
 
-
+Route::middleware('auth')->group(function () {
+    Route::middleware('admin')->group(function () {
+        Route::resource('category', CategoryController::class);
+        Route::resource('application', ApplicationController::class)->only(['update']);
+        Route::get('admin', [AdminController::class, 'index'])->name('admin');
+    });
+    Route::resource('application', ApplicationController::class)->only(['index', 'create', 'store', 'destroy']);
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
